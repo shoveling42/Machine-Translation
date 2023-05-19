fbdf456b41e529cd23d3f206d16e380cb42a668d
Junseok Lee <behindstarter42@gmail.com>
2023-05-19 14:13:27 +0900

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,8 @@
 import json
+from typing import List, Tuple
+from torch.utils.data import DataLoader
 
+# extract_en, extract_ko functions are deprecated
 def extract_en(file_path: str, type: str, num: int):
     with open(file_path, 'r', encoding='utf-8') as file:
         sents = json.load(file)
@@ -16,18 +19,19 @@ def extract_ko(file_path: str, type: str, num: int):
             for sent in sents["data"]:
                 f.write(sent["ko"] + '\n')
 
-train = 'train'
-valid = 'valid'
+def create_parallel(file_path: str) -> List[Tuple]:
+    form = list()
+    with open(file_path, 'r', encoding='utf-8') as file:
+        sents = json.load(file)
+        for sent in sents["data"]:
+            temp = (sent["en"], sent["ko"])
+            form.append(temp)
+        return form
 
-def main():
-    file_path = r'' # input file path
-    '''
-    type: train, valid
-    num: index of dataset
-    '''
-    extract_en(file_path, type=valid, num=3)
-    extract_ko(file_path, type=valid, num=3)
-    return 0
+train_file = r'' # train_set.json file_path
+valid_file = r'' # valid_set.json file_path
 
-if __name__ == "__main__":
-    main()
+train_iter = create_parallel(train_file)
+valid_iter = create_parallel(valid_file)
+train_dataloader = DataLoader(train_iter, batch_size=64)
+valid_dataloader = DataLoader(valid_iter, batch_size=64)
